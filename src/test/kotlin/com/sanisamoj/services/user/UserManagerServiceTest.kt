d5ec38e9f33fd87c23c9f34fd9c5524bf81ab395
João Mário <marioartec39@gmail.com>
2024-09-02 19:03:10 -0300

    UpdatePassword test

--- src/test/kotlin/com/sanisamoj/services/user/UserManagerServiceTest.kt
+++ src/test/kotlin/com/sanisamoj/services/user/UserManagerServiceTest.kt
@@ -1,6 +1,7 @@
 package com.sanisamoj.services.user
 
 import com.sanisamoj.TestContext
+import com.sanisamoj.data.models.dataclass.LoginRequest
 import com.sanisamoj.data.models.dataclass.User
 import com.sanisamoj.data.models.dataclass.UserResponse
 import com.sanisamoj.data.models.enums.AccountStatus
@@ -40,13 +41,40 @@ class UserManagerServiceTest {
         val user: User = userTest.createUserTest(accountStatus = AccountStatus.Active)
         val userId: String = user.id.toString()
         val userManagerService = UserManagerService(databaseRepository = databaseRepository, botRepository = botRepository)
-        userManagerService.updatePhone(user.id.toString(), "1111111111111")
+        userManagerService.updatePhone(userId, "1111111111111")
 
-        val updatedUser: User = databaseRepository.getUserById(user.id.toString())
+        val updatedUser: User = databaseRepository.getUserById(userId)
         assertFails { userManagerService.validateValidationCodeToUpdatePhone(userId, "1111111111111", 123456) }
 
         val updatedUserResponse: UserResponse = userManagerService.validateValidationCodeToUpdatePhone(userId, "1111111111111", updatedUser.validationCode)
         assertEquals("1111111111111", updatedUserResponse.phone)
+        userTest.deleteUserTest()
+    }
+
+    @Test
+    fun updatePasswordTest() = testApplication {
+        val userTest = UserTest()
+        val user: User = userTest.createUserTest(accountStatus = AccountStatus.Active)
+        val userId: String = user.id.toString()
+        val userManagerService = UserManagerService(databaseRepository = databaseRepository, botRepository = botRepository)
+        userManagerService.updatePassword(userId)
+
+        val updatedUser: User = databaseRepository.getUserById(userId)
+        assertFails { userManagerService.validateValidationCodeToUpdatePassword(userId, "1111111111111", 123456) }
+
+        userManagerService.validateValidationCodeToUpdatePassword(userId, "1111111111111", updatedUser.validationCode)
+        val userAuthenticationService = UserAuthenticationService(
+            databaseRepository = TestContext.getDatabaseRepository(),
+            mailRepository = TestContext.getMailRepository()
+        )
+
+        val loginRequest = LoginRequest(user.email, "1111111111111")
+        val loginResponse = userAuthenticationService.login(loginRequest)
+
+        assertEquals(user.email, loginResponse.account.email, "Email data comparison")
+        assertEquals(updatedUser.id.toString(), loginResponse.account.id, "ID data comparison")
+        assertEquals(updatedUser.accountStatus, AccountStatus.Active.name, "Account Status data comparison")
+
         userTest.deleteUserTest()
     }
 }
