c4d5c9516b59796baa1575f8ad614bec44b250ed
João Mário <marioartec39@gmail.com>
2024-10-03 00:42:20 -0300

    v0.27.1

--- src/main/kotlin/com/sanisamoj/plugins/RateLimit.kt
+++ src/main/kotlin/com/sanisamoj/plugins/RateLimit.kt
@@ -13,11 +13,11 @@ import kotlin.time.Duration.Companion.seconds
 fun Application.rateLimit() {
     install(RateLimit) {
         register(RateLimitName("register")) {
-            rateLimiter(limit = 2, refillPeriod = 1.hours)
+            rateLimiter(limit = 10, refillPeriod = 1.hours)
         }
 
         register(RateLimitName("validation")) {
-            rateLimiter(limit = 5, refillPeriod = 24.hours)
+            rateLimiter(limit = 10, refillPeriod = 24.hours)
         }
 
         register(RateLimitName("lightweight")) {
@@ -25,11 +25,11 @@ fun Application.rateLimit() {
         }
 
         register(RateLimitName("login")) {
-            rateLimiter(limit = 5, refillPeriod = 1.hours)
+            rateLimiter(limit = 10, refillPeriod = 1.hours)
         }
 
         register(RateLimitName("publicLinkEntry")) {
-            rateLimiter(limit = 20, refillPeriod = 1.minutes)
+            rateLimiter(limit = 1000, refillPeriod = 1.minutes)
 
             modifyResponse { applicationCall, state ->
                 when (state) {
